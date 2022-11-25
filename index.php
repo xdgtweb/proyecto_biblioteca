300a624487ed3579a72527ae5bffb8d7fed8430f
xdgtweb <110548730+xdgtweb@users.noreply.github.com>
2022-11-25 16:55:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 require_once "./views/index.view.php";
 include "./views/contact.view.php";
 include "./views/index.view.php";
+
+hola como puedo contactar contigo? XD te paso mi instagram, manda un mensaje porfa gracovitz
